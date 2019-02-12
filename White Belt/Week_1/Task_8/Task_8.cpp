0a86678b078a66b808ae171a468d16a554c53aa0
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 00:15:12 +0400

    Наибольший общий делитель

---
+++ White Belt/Week_1/Task_8/Task_8.cpp
@@ -0,0 +1,29 @@
+﻿// Task_8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+/*
+ * В stdin даны два натуральных числа.
+ * Выведите в stdout их наибольший общий делитель.
+ */
+int main()
+{
+	int number1, number2;
+
+	cin >> number1 >> number2;
+
+	while (number1 > 0 && number2 > 0)
+	{
+		if (number1 > number2)
+			number1 %= number2;
+		else
+			number2 %= number1;
+	}
+
+	cout << number1 + number2;
+}
+
