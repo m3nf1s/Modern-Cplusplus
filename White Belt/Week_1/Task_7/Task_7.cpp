edc38d889dda68eb07969a69bd27f271feeb27f6
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-12 23:34:56 +0400

    Второе вхождение

---
+++ White Belt/Week_1/Task_7/Task_7.cpp
@@ -0,0 +1,42 @@
+﻿// Task_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+/* Дана строка.
+ * Найдите в этой строке второе вхождение буквы f и выведите индекс этого вхождения.
+ * Если буква f в данной строке встречается только один раз, выведите число -1, а если не встречается ни разу, выведите число -2.
+ * Индексы нумеруются с нуля.
+ */
+int main()
+{
+	string word;
+	cin >> word;
+	int itr = 0;
+	int buff;
+	for (int i = 0; i < word.size(); i++)
+	{
+		if (word[i] == 'f')
+		{
+			itr++;
+			if (itr == 2)
+			{
+				buff = i;
+				break;
+			}
+		}
+	}
+
+	if (itr == 2)
+		cout << buff << endl;
+	else
+		if (itr == 1)
+			cout << -1 << endl;
+		else
+			cout << -2 << endl;
+		
+	return 0;
+}
