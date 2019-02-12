b34a86e25dab545d26a46b74fcf69296d9f63633
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 00:14:33 +0400

    Двоичные числа

---
+++ White Belt/Week_1/Task_9/Task_9.cpp
@@ -0,0 +1,35 @@
+﻿// Task_9.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* На вход дано целое положительное число N.
+ * Выведите его в двоичной системе счисления без ведущих нулей.
+ */
+int main()
+{
+	vector<int> vec;
+	int number;
+	string a;
+
+	cin >> number;
+
+	while (number != 0)
+	{
+		vec.push_back(number % 2);
+		number /= 2;
+	}
+
+	for (int i = vec.size() - 1; i >= 0; i--)
+	{
+		cout << vec[i];
+	}
+
+	return 0;
+}
+
+
