5cf5174ff2a1697c153844e2975017cf401dc452
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 17:14:21 +0400

    Палиндром

---
+++ White Belt/Week_2/Task_2/Task_2.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*
+ * Напишите функцию, которая
+ * называется IsPalindrom
+ * возвращает bool
+ * принимает параметр типа string и возвращает, является ли переданная строка палиндромом
+ * Палиндром - это слово или фраза, которые одинаково читаются слева направо и справа налево.
+ * Пустая строка является палиндромом.
+ */
+
+bool IsPalindrom (string word)
+{
+	for (size_t i = 0; i < word.size() / 2; i++)
+	{
+		if (word[i] != word[word.size() - i - 1])
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	string word;
+	cin >> word;
+	cout << IsPalindrom(word);
+
+	return 0;
+}
