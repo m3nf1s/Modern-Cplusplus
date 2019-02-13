fb49200f763310cf64044c6e5b52a93eb3a119ca
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 22:06:36 +0400

    Разворот последовательности — 2

---
+++ White Belt/Week_2/Task_7/Task_7.cpp
@@ -0,0 +1,30 @@
+﻿// Task_7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+ * Реализуйте функцию vector<int> Reversed(const vector<int>& v), возвращающую копию вектора v, в которой числа переставлены в обратном порядке.
+ */
+
+vector<int> Reversed(const vector<int>& v)
+{
+	vector<int> buff;
+	for (int i = v.size() - 1; i >= 0; i--)
+		buff.push_back(v[i]);
+	return buff;
+}
+
+int main()
+{
+	vector<int> test = { 1, 5, 3, 4, 2 };
+	vector<int> simple = Reversed(test);
+
+	for (auto n : simple)
+		cout << n << " ";
+}
