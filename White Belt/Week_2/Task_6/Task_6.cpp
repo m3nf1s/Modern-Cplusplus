3058ed70dffffec1b1cefce1c9eb8375b211c816
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 22:06:09 +0400

    Разворот последовательности

---
+++ White Belt/Week_2/Task_6/Task_6.cpp
@@ -0,0 +1,49 @@
+﻿// Task_6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+ * Реализуйте функцию void Reverse(vector<int>& v), которая переставляет элементы вектора в обратном порядке.
+ */
+
+void ReverseV1 (vector<int>& v)
+{
+	for (int i = 0; i < v.size() / 2 ; i++)
+	{
+		int buff = v[i];
+		v[i] = v[v.size() - i - 1];
+		v[v.size() - i - 1] = buff;
+	}
+}
+
+void ReverseV2 (vector<int>& v)
+{
+	vector<int> buff;
+	for (int i = v.size() - 1; i >= 0; i--)
+		buff.push_back(v[i]);
+	v.clear();
+	v = buff;
+}
+
+int main()
+{
+	vector<int> test = { 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	ReverseV1 (test);
+
+	for (auto n : test)
+		cout << n << " ";
+
+	cout << endl;
+
+	ReverseV2(test);
+
+	for (auto n : test)
+		cout << n << " ";
+}
+
