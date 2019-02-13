82e6639b6694c0ce84d582cff710f6df7ebccb0c
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-13 20:16:31 +0400

    Перемещение строк

---
+++ White Belt/Week_2/Task_5/Task_5.cpp
@@ -0,0 +1,38 @@
+﻿// Task_5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/* Напишите функцию MoveStrings, которая принимает два вектора строк, source и destination, и дописывает все строки из первого вектора в конец второго.
+ * После выполнения функции вектор source должен оказаться пустым.
+ */
+
+void MoveString(vector<string>& source, vector<string>& destination)
+{
+	for (auto w : source)
+		destination.push_back(w);
+
+	source.clear();
+}
+
+int main()
+{
+	vector<string> first = { "qwe", "rty", "asd", "fgh" };
+	vector<string> second = { "keyboard: " };
+	MoveString(first, second);
+
+	cout << "first - ";
+	for (auto w : first)
+		cout <<  w ;
+
+	cout << endl;
+
+	cout << "second - ";
+	for (auto w : second)
+		cout << w;
+}
