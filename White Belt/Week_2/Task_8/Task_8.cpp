27f4a957995cb92a7893cc102caaa01d8415f30d
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-02-14 11:18:07 +0400

    Средняя температура

---
+++ White Belt/Week_2/Task_8/Task_8.cpp
@@ -0,0 +1,51 @@
+﻿// Task_8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+/*
+ * Даны значения температуры, наблюдавшиеся в течение N подряд идущих дней.
+ * Найдите номера дней (в нумерации с нуля) со значением температуры выше среднего арифметического за все N дней.
+ * Гарантируется, что среднее арифметическое значений температуры является целым числом.
+ * 
+ * Формат ввода
+ * Вводится число N, затем N неотрицательных целых чисел — значения температуры в 0-й, 1-й, ... (N−1)-й день.
+ * 
+ * Формат вывода
+ * Первое число K — количество дней, значение температуры в которых выше среднего арифметического.
+ * Затем K целых чисел — номера этих дней.
+ */
+int main()
+{
+	int length;
+	int sum = 0;
+	int days = 0;
+	cin >> length;
+
+	vector<int> temperatures(length);
+
+	for (int& temperature : temperatures)
+	{
+		cin >> temperature;
+		sum += temperature;
+	}
+
+	int average = sum / length;
+
+	for (int temperature : temperatures)
+		if (temperature > average)
+			days++;
+
+	cout << days << endl;
+
+	for (int i = 0; i < temperatures.size(); i++)
+		if (temperatures[i] > average)
+			cout << i << " ";
+
+	return 0;
+}
