bc0028b82f11871a183bbe27ee63db31c7379140
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-03-25 02:42:34 +0400

    Множество значений словаря

---
+++ White Belt/Week_2/Task_16/Task_16.cpp
@@ -0,0 +1,40 @@
+﻿// Task_16.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+/*
+ * Напишите функцию BuildMapValuesSet, принимающую на вход словарь map<int, string> и возвращающую множество значений этого словаря:
+ */
+#include "pch.h"
+#include <iostream>
+#include <string>
+#include <map>
+#include <set>
+
+std::set<std::string> BuildMapValuesSet (const std::map<int, std::string>& m)
+{
+	std::set<std::string> result;
+	for (const auto& item : m)
+	{
+		result.insert(item.second);
+	}
+
+	return result;
+}
+
+int main()
+{
+	std::set<std::string> values = BuildMapValuesSet(
+		{
+			{1,"odd"},
+			{2,"even"},
+			{3,"odd"},
+			{4,"even"},
+			{5,"odd"}
+		});
+
+	for (const auto& value : values)
+	{
+		std::cout << value << std::endl;
+	}
+}
+
