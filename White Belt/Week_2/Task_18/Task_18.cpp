ccdb5bb1ad3a68dfc7e7de0ba11f637c5e9094de
m3nf1s <39036679+m3nf1s@users.noreply.github.com>
2019-03-25 16:26:21 +0400

    Автобусные остановки — 3

---
+++ White Belt/Week_2/Task_18/Task_18.cpp
@@ -0,0 +1,45 @@
+﻿// Task_18.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+#include <set>
+#include <string>
+#include <map>
+
+int main()
+{
+	std::map<std::set<std::string>, int> list_bus_stops;
+	int command_count, number_route = 0;
+
+	std::cin >> command_count;
+
+	for (int i = 0; i < command_count; ++i)
+	{
+		int stops_count;
+		std::set<std::string> stops;
+
+		std::cin >> stops_count;
+
+		for (int j = 0; j < stops_count; ++j)
+		{
+			std::string stop_name;
+			std::cin >> stop_name;
+
+			stops.insert(stop_name);
+		}
+
+		if(list_bus_stops.count(stops) == 1)
+		{
+			std::cout << "Already exists for " << list_bus_stops[stops] << std::endl;
+		}
+		else
+		{
+			list_bus_stops[stops] = ++number_route;
+			std::cout << "New bus " << number_route << std::endl;
+		}
+
+		stops.clear();
+	}
+}
+
