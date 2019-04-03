b9f404b9737ce8e34a46eaeadee16d6043f79184
Andrey <titan94@mail.ru>
2019-04-03 17:31:06 +0400

    Отсортированные строки

---
+++ White Belt/Week_3/Task_3/Task_3.cpp
@@ -0,0 +1,25 @@
+﻿// Task_3.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+struct Lecture
+{
+	std::string title;
+	int duration;
+	std::string author;
+};
+
+void PrintLecture(const Lecture& lec)
+{
+	std::cout << lec.title << " " << lec.duration << " " << lec.author << std::endl;
+}
+
+
+int main()
+{
+	Lecture math = { "Math", 20, "Hennry" };
+	PrintLecture(math);
+}
+
