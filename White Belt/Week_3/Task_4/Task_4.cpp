b16ef2a3aadab1be6ed8128bd556518cd0289d4b
Andrey <titan94@mail.ru>
2019-04-05 17:03:33 +0400

    Имена и фамилии — 1

---
+++ White Belt/Week_3/Task_4/Task_4.cpp
@@ -0,0 +1,30 @@
+﻿// Task_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+#include <map>
+
+class Person
+{
+public:
+	void ChangeFirstName(int year, const std::string& firts_name)
+	{
+
+	}
+	void ChangeLastName(int year, const std::string& last_name)
+	{
+
+	}
+	std::string GetFullName(int year)
+	{
+
+	}
+private:
+
+};
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
