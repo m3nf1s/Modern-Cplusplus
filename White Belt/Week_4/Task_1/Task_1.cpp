42b4c8a011f5213e83f0da962ba0805a5f87790d
Andrey <titan94@mail.ru>
2019-04-11 17:32:06 +0400

    Структура Date

---
+++ White Belt/Week_4/Task_1/Task_1.cpp
@@ -0,0 +1,57 @@
+﻿// Task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+struct Specialization
+{
+public:
+	explicit Specialization(std::string new_value)
+	{
+		value = new_value;
+	}
+	std::string value;
+};
+
+struct Course
+{
+public:
+	Course(std::string new_value)
+	{
+		value = new_value;
+	}
+	std::string value;
+};
+
+struct Week
+{
+public:
+	Week(std::string new_value)
+	{
+		value = new_value;
+	}
+	std::string value;
+};
+
+struct LectureTitle
+{
+public:
+	LectureTitle(Specialization spec_name, Course course_name, Week week_name)
+	{
+		specialization = spec_name.value;
+		course = course_name.value;
+		week = week_name.value;
+	}
+	std::string specialization;
+	std::string course;
+	std::string week;	
+};
+int main()
+{
+	LectureTitle title(
+		Specialization("C++"),
+		Course("White belt"),
+		Week("4th")
+	);
+}
