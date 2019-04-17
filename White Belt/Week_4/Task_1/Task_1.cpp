b105e9e98e571bebe627c0237521fa31dce1f462
Andrey <titan94@mail.ru>
2019-04-17 17:24:53 +0400

    Структура Date

--- White Belt/Week_4/Task_1/Task_1.cpp
+++ White Belt/Week_4/Task_1/Task_1.cpp
@@ -1,9 +1,43 @@
-﻿// Task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
+﻿#include "pch.h"
 #include <iostream>
 #include <string>
+
+/*
+Дана структура LectureTitle:
+	struct LectureTitle {
+	  string specialization;
+	  string course;
+	  string week;
+	};
+Допишите конструктор и структуры Specialization, Course, Week так, чтобы объект LectureTitle можно было создать с помощью кода
+	LectureTitle title(
+	Specialization("C++"),
+	Course("White belt"),
+	Week("4th")
+	);
+но нельзя было с помощью следующих фрагментов кода:
+
+LectureTitle title("C++", "White belt", "4th");
+
+LectureTitle title(string("C++"), string("White belt"), string("4th"));
+
+LectureTitle title = {"C++", "White belt", "4th"};
+
+LectureTitle title = {{"C++"}, {"White belt"}, {"4th"}};
+
+LectureTitle title(
+	Course("White belt"),
+	Specialization("C++"),
+	Week("4th")
+);
+
+LectureTitle title(
+	Specialization("C++"),
+	Week("4th"),
+	Course("White belt")
+);	
+
+*/
 
 struct Specialization
 {
