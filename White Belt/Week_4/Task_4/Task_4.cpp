4925919339c41c7f15ae3b6be97ca45158c5417e
Andrey <titan94@mail.ru>
2019-04-17 22:10:39 +0400

    Работа с файлами 1

---
+++ White Belt/Week_4/Task_4/Task_4.cpp
@@ -0,0 +1,27 @@
+﻿/*
+ * В этом задании вам предстоит написать две небольшие программы.
+ * Каждый пункт - отдельная задача, решение отправляйте в поле с соответствующим номером.
+ * 
+ * Часть 1
+ * Ваша программа должна считать содержимое файла input.txt и напечатать его на экран без изменений.
+ * Гарантируется, что содержимое файла input.txt заканчивается переводом строки.
+ */
+
+#include "pch.h"
+#include <fstream>
+#include <string>
+#include <iostream>
+
+int main()
+{
+	std::ifstream input("input.txt");
+
+	if (input.is_open())
+	{
+		std::string line;
+		while (std::getline(input, line))
+		{
+			std::cout << line << std::endl;
+		}
+	}
+}
