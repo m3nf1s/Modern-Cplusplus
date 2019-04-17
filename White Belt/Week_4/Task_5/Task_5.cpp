2c702a34e78c907b57fd451263eaeee934b161c0
Andrey <titan94@mail.ru>
2019-04-17 22:10:48 +0400

    Работа с файлами 2

---
+++ White Belt/Week_4/Task_5/Task_5.cpp
@@ -0,0 +1,26 @@
+﻿/*
+ * В этом задании вам предстоит написать две небольшие программы.
+ * Каждый пункт - отдельная задача, решение отправляйте в поле с соответствующим номером.
+ * 
+ * Часть 2
+ * Снова считайте все содержимое файла input.txt, но на этот раз выведите его в файл output.txt.
+ * Точно так же гарантируется, что содержимое файла input.txt заканчивается переводом строки.
+ */
+#include "pch.h"
+#include <fstream>
+#include <string>
+
+int main()
+{
+	std::fstream input("input.txt");
+	std::ofstream output("output.txt");
+
+	if(input)
+	{
+		std::string line;
+		while (std::getline(input, line))
+		{
+			output << line << std::endl;			
+		}
+	}
+}
