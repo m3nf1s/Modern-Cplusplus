43663122153037067c59a08054801290580335ae
Andrey <titan94@mail.ru>
2019-04-22 17:19:59 +0400

    Класс Rational 2

--- White Belt/Week_4/Task_10/Task_10.cpp
+++ White Belt/Week_4/Task_10/Task_10.cpp
@@ -49,17 +49,17 @@ public:
 		}
 		else
 		{
-			int NOD = FindGCD(numerator, denominator);
+			int GCD = FindGCD(numerator, denominator);
 
 			if ((numerator > 0 && denominator < 0) || (numerator < 0 && denominator < 0))
 			{
-				_numerator = -numerator / NOD;
-				_denominator = -denominator / NOD;
+				_numerator = -numerator / GCD;
+				_denominator = -denominator / GCD;
 			}
 			else
 			{
-				_numerator = numerator / NOD;
-				_denominator = denominator / NOD;
+				_numerator = numerator / GCD;
+				_denominator = denominator / GCD;
 			}
 		}
 	}
@@ -102,7 +102,7 @@ private:
 
 bool operator== (const Rational& one, const Rational& two)
 {
-	return (one.Numerator() / one.Denominator()) == (two.Numerator() / two.Denominator());
+	return ((double)one.Numerator() / one.Denominator()) == ((double)two.Numerator() / two.Denominator());
 }
 
 Rational operator+ (const Rational& one, const Rational& two)
