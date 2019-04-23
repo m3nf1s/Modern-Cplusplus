7db989f85223a73ba764256f13ce14cab038bfa9
Andrey <titan94@mail.ru>
2019-04-23 10:17:01 +0400

    Финальная задача курса (Первое кумитэ)

---
+++ White Belt/Week_5 (Final_project)/Week_5 (Final_project).cpp
@@ -0,0 +1,10 @@
+﻿// Week_5 (Final_project).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
