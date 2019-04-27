b83c985fc204648c45ed6fe8ca65938c3f381bda
Andrey <titan94@mail.ru>
2019-04-27 17:54:20 +0400

    Способы инициализации

--- White Belt/Week_3/Task_8/Task_8.cpp
+++ White Belt/Week_3/Task_8/Task_8.cpp
@@ -1,9 +1,7 @@
-﻿// Task_8.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-/*
-Определите тип Incognizable, для которого следующий код будет корректен:
+﻿/*
+	Определите тип Incognizable, для которого следующий код будет корректен:
 */
+
 #include <iostream>
 
 class Incognizable
@@ -14,16 +12,19 @@ public:
 		x = 0;
 		y = 0;
 	};
+	
 	Incognizable(int a)
 	{
 		x = a;
 		y = 0;
 	};
+	
 	Incognizable(int a, int b) 
 	{
 		x = a;
 		y = b;
 	};
+	
 private:
 	int x;
 	int y;
@@ -35,4 +36,3 @@ int main()
 	Incognizable c = { 0 };
 	Incognizable d = { 0, 1 };
 }
-
