0edc835a7aeec644e83d949d2387396b9db5a9af
Andrey <titan94@mail.ru>
2019-04-27 17:49:31 +0400

    Максимизатор

--- White Belt/Week_2/Task_4/Task_4.cpp
+++ White Belt/Week_2/Task_4/Task_4.cpp
@@ -1,16 +1,12 @@
-﻿// Task_4.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include "pch.h"
-#include <iostream>
-
-using namespace std;
-
-/*
+﻿/*
  * Напишите функцию UpdateIfGreater, которая принимает два целочисленных аргумента: first и second.
  * Если first оказался больше second, Ваша функция должна записывать в second значение параметра first.
  * При этом изменение параметра second должно быть видно на вызывающей стороне.
  */
+ 
+#include <iostream>
+
+using namespace std;
 
 void UpdateIfGreater (const int& number1, int& number2)
 {
@@ -25,4 +21,3 @@ int main()
 	UpdateIfGreater(number1, number2);
 	cout << number1 << " " << number2 << endl;
 }
-
