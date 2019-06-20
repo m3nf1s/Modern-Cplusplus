7ef5336c14bc310c2f54b0b239e221413d918964
Andrey <titan94@mail.ru>
2019-06-20 09:02:06 +0400

    Часть вектора

---
+++ Yellow Belt/Week_4/Task_1/Task_1.cpp
@@ -0,0 +1,35 @@
+﻿/*
+Напишите функцию PrintVectorPart, принимающую вектор целых чисел numbers,
+	выполняющую поиск первого отрицательного числа в нём и выводящую в стандартный вывод все числа,
+	расположенные левее найденного, в обратном порядке.
+Если вектор не содержит отрицательных чисел, выведите все числа в обратном порядке.
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void PrintVectorPart(const std::vector<int>& numbers)
+{
+	auto it = std::find_if(numbers.begin(), numbers.end(), [](int x)
+		{
+			return x < 0;
+		});
+
+	while (it != numbers.begin())
+	{
+		--it;
+		std::cout << *it << ' ';
+	}
+}
+
+int main()
+{
+	PrintVectorPart({ 6, 1, 8, -5, 4 });
+	std::cout << std::endl;
+	PrintVectorPart({ -6, 1, 8, -5, 4 });  // ничего не выведется
+	std::cout << std::endl;
+	PrintVectorPart({ 6, 1, 8, 5, 4 });
+	std::cout << std::endl;
+	return 0;
+}
