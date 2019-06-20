1a9466ceca6f15b000b34777d7cc5d336d43f2e1
Andrey <titan94@mail.ru>
2019-06-20 09:02:43 +0400

    Часть множества

---
+++ Yellow Belt/Week_4/Task_2/FindGreaterElements.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <algorithm>
+
+template <typename T>
+std::vector<T> FindGreaterElements(const std::set<T>& elements, const T& border);
+
+template <typename T>
+std::vector<T> FindGreaterElements(const std::set<T>& elements, const T& border)
+{
+	auto it = std::find_if(elements.begin(), elements.end(), [border] (T n)
+		{
+			return n > border;
+		});
+
+	return { it, elements.end() };
+}
---
+++ Yellow Belt/Week_4/Task_2/Task_2.cpp
@@ -0,0 +1,24 @@
+﻿/*
+Напишите шаблонную функцию FindGreaterElements,
+	принимающую множество elements объектов типа T и ещё один объект border типа T и возвращающую вектор из всех элементов множества,
+	бо́льших border, в возрастающем порядке.
+*/
+
+#include <iostream>
+#include "FindGreaterElements.h"
+
+int main() {
+	for (int x : FindGreaterElements(std::set<int>{1, 5, 7, 8}, 5)) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+
+	for (int x : FindGreaterElements(std::set<int>{1, 5, 7, 8}, 0)) {
+		std::cout << x << " ";
+	}
+	std::cout << std::endl;
+
+	std::string to_find = "Python";
+	std::cout << FindGreaterElements(std::set<std::string>{"C", "C++"}, to_find).size() << std::endl;
+	return 0;
+}
