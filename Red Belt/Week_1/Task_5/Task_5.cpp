a1b792b0206470fa449cf589f21521ae51334a07
Andrey <titan94@mail.ru>
2019-11-20 01:47:06 +0400

    Плохой макрос

---
+++ Red Belt/Week_1/Task_5/Task_5.cpp
@@ -0,0 +1,24 @@
+﻿#include "test_runner.h"
+
+#include <ostream>
+
+using namespace std;
+
+#define PRINT_VALUES(out, x, y)								\
+do															\
+{															\
+	out << (x) << endl; 									\
+	out << (y) << endl;										\
+	break;													\
+} while (true)
+
+int main()
+{
+	TestRunner tr;
+	tr.RunTest([]
+		{
+			ostringstream output;
+			PRINT_VALUES(output, 5, "red belt");
+			ASSERT_EQUAL(output.str(), "5\nred belt\n");
+		}, "PRINT_VALUES usage example");
+}
