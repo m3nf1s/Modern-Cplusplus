76109a8f21276d0756bde12f2cc53d6dad37715a
Andrey <titan94@mail.ru>
2019-11-20 01:46:35 +0400

    Генератор компараторов

---
+++ Red Belt/Week_1/Task_3/Task_3.cpp
@@ -0,0 +1,43 @@
+﻿#include "airline_ticket.h"
+#include "test_runner.h"
+
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+#define SORT_BY(field)											\
+	[](const AirlineTicket& lhs, const AirlineTicket& rhs)		\
+	{															\
+		return lhs.field < rhs.field;							\
+	}															
+
+
+void TestSortBy()
+{
+	vector<AirlineTicket> tixs =
+	{
+	  {"VKO", "AER", "Utair",     {2018, 2, 28}, {17, 40}, {2018, 2, 28}, {20,  0}, 1200},
+	  {"AER", "VKO", "Utair",     {2018, 3,  5}, {14, 15}, {2018, 3,  5}, {16, 30}, 1700},
+	  {"AER", "SVO", "Aeroflot",  {2018, 3,  5}, {18, 30}, {2018, 3,  5}, {20, 30}, 2300},
+	  {"PMI", "DME", "Iberia",    {2018, 2,  8}, {23, 00}, {2018, 2,  9}, { 3, 30}, 9000},
+	  {"CDG", "SVO", "AirFrance", {2018, 3,  1}, {13, 00}, {2018, 3,  1}, {17, 30}, 8000},
+	};
+
+	sort(begin(tixs), end(tixs), SORT_BY(price));
+	ASSERT_EQUAL(tixs.front().price, 1200);
+	ASSERT_EQUAL(tixs.back().price, 9000);
+
+	sort(begin(tixs), end(tixs), SORT_BY(from));
+	ASSERT_EQUAL(tixs.front().from, "AER");
+	ASSERT_EQUAL(tixs.back().from, "VKO");
+
+	sort(begin(tixs), end(tixs), SORT_BY(arrival_date));
+	ASSERT_EQUAL(tixs.front().arrival_date, (Date{ 2018, 2, 9 }));
+	ASSERT_EQUAL(tixs.back().arrival_date, (Date{ 2018, 3, 5 }));
+}
+
+int main() {
+	TestRunner tr;
+	RUN_TEST(tr, TestSortBy);
+}
---
+++ Red Belt/Week_1/Task_3/airline_ticket.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <string>
+#include <tuple>
+#include <iostream>
+
+using namespace std;
+
+struct Date
+{
+	int year, month, day;
+};
+
+struct Time
+{
+	int hours, minutes;
+};
+
+struct AirlineTicket
+{
+	string from;
+	string to;
+	string airline;
+	Date departure_date;
+	Time departure_time;
+	Date arrival_date;
+	Time arrival_time;
+	int price;
+};
+
+bool operator< (const Date& lhs, const Date& rhs)
+{
+	return std::tie(lhs.year, lhs.month, lhs.day) < std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+bool operator== (const Date& lhs, const Date& rhs)
+{
+	return std::tie(lhs.year, lhs.month, lhs.day) == std::tie(rhs.year, rhs.month, rhs.day);
+}
+
+std::ostream& operator<< (std::ostream& os, const Date& date)
+{
+	return os << date.year << ", " << date.month << ", " << date.day;
+}
+
+
+bool operator< (const Time& lhs, const Time& rhs)
+{
+	return std::tie(lhs.hours, lhs.minutes) < std::tie(rhs.hours, rhs.minutes);
+}
+
+bool operator== (const Time& lhs, const Time& rhs)
+{
+	return std::tie(lhs.hours, lhs.minutes) == std::tie(rhs.hours, rhs.minutes);
+}
+
+std::ostream& operator<< (std::ostream& os, const Time& time)
+{
+	return os << time.hours << time.minutes;
+}
