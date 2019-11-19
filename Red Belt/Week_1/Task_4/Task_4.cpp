eb0bcfbdffe60888e3b4acbe96f7d5f2639e31de
Andrey <titan94@mail.ru>
2019-11-20 01:46:44 +0400

    Макрос UPDATE_FIELD

---
+++ Red Belt/Week_1/Task_4/Task_4.cpp
@@ -0,0 +1,55 @@
+﻿#include "airline_ticket.h"
+#include "test_runner.h"
+
+using namespace std;
+
+#define UPDATE_FIELD(ticket, field, values)		\
+{												\
+	auto it = values.find(#field);				\
+	if (it != values.end())						\
+	{											\
+		std::istringstream is (it->second);		\
+		is >> ticket.field;						\
+	}											\
+}
+
+void TestUpdate()
+{
+	AirlineTicket t;
+	t.price = 0;
+
+	const map<string, string> updates1 =
+	{
+	  {"departure_date", "2018-2-28"},
+	  {"departure_time", "17:40"},
+	};
+	UPDATE_FIELD(t, departure_date, updates1);
+	UPDATE_FIELD(t, departure_time, updates1);
+	UPDATE_FIELD(t, price, updates1);
+
+	ASSERT_EQUAL(t.departure_date, (Date{ 2018, 2, 28 }));
+	ASSERT_EQUAL(t.departure_time, (Time{ 17, 40 }));
+	ASSERT_EQUAL(t.price, 0);
+
+	const map<string, string> updates2 =
+	{
+	  {"price", "12550"},
+	  {"arrival_time", "20:33"},
+	};
+	UPDATE_FIELD(t, departure_date, updates2);
+	UPDATE_FIELD(t, departure_time, updates2);
+	UPDATE_FIELD(t, arrival_time, updates2);
+	UPDATE_FIELD(t, price, updates2);
+
+	// updates2 не содержит ключей "departure_date" и "departure_time", поэтому
+	// значения этих полей не должны измениться
+	ASSERT_EQUAL(t.departure_date, (Date{ 2018, 2, 28 }));
+	ASSERT_EQUAL(t.departure_time, (Time{ 17, 40 }));
+	ASSERT_EQUAL(t.price, 12550);
+	ASSERT_EQUAL(t.arrival_time, (Time{ 20, 33 }));
+}
+
+int main() {
+	TestRunner tr;
+	RUN_TEST(tr, TestUpdate);
+}
