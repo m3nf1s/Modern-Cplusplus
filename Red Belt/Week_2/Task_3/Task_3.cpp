8dfbf02cdeca281064b6661a6260a2c6cc2f9718
Andrey <titan94@mail.ru>
2019-11-30 23:23:50 +0400

    Экспрессы

--- Red Belt/Week_2/Task_3/Task_3.cpp
+++ Red Belt/Week_2/Task_3/Task_3.cpp
@@ -9,31 +9,31 @@
 class RouteManager
 {
 public:
-	void AddRoute(int start, int finish)										// Total: O(log(Q))					
+	void AddRoute(int start, int finish)						// Total: O(log(Q))					
 	{
-		reachable_lists_[start].insert(finish);									// O(log(Q) + log(Q))	
-		reachable_lists_[finish].insert(start);									// O(log(Q) + log(Q))	
+		reachable_lists_[start].insert(finish);					// O(log(Q) + log(Q))	
+		reachable_lists_[finish].insert(start);					// O(log(Q) + log(Q))	
 	}
 
-	int FindNearestFinish(int start, int finish) const							// Total : O(log(Q))	
+	int FindNearestFinish(int start, int finish) const				// Total : O(log(Q))	
 	{
-		int result = abs(start - finish);										// O(1)
-		if (reachable_lists_.count(start) < 1)									// O(log(Q))	
+		int result = abs(start - finish);					// O(1)
+		if (reachable_lists_.count(start) < 1)					// O(log(Q))	
 		{  
 			return result;
 		}
 
 		const std::set<int>& reachable_stations = reachable_lists_.at(start);	// O(1)
 
-		const auto finish_pos = reachable_stations.lower_bound(finish);			// O(logQ)
+		const auto finish_pos = reachable_stations.lower_bound(finish);		// O(logQ)
 
-		if (finish_pos != reachable_stations.end())								// O(1)
+		if (finish_pos != reachable_stations.end())				// O(1)
 		{
-			result = std::min(result, abs(finish - *finish_pos));				// O(1)
+			result = std::min(result, abs(finish - *finish_pos));		// O(1)
 		}
-		if (finish_pos != reachable_stations.begin())							// O(1)
+		if (finish_pos != reachable_stations.begin())				// O(1)
 		{
-			result = std::min(result, abs(finish - *prev(finish_pos)));			// O(1)
+			result = std::min(result, abs(finish - *prev(finish_pos)));	// O(1)
 		} 
 
 		return result;
@@ -44,7 +44,7 @@ private:
 };
 
 
-int main()																		// Total: O(Q log(Q))
+int main()										// Total: O(Q log(Q))
 {
 
 	std::ios_base::sync_with_stdio(false);
@@ -55,7 +55,7 @@ int main()																		// Total: O(Q log(Q))
 	int query_count;
 	std::cin >> query_count;
 
-	for (int query_id = 0; query_id < query_count; ++query_id)					// O(Q)
+	for (int query_id = 0; query_id < query_count; ++query_id)			// O(Q)
 	{
 		std::string query_type;
 		std::cin >> query_type;
