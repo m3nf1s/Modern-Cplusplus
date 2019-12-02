8e4251c2a8a0ab1433598ad96de8c76de3975df6
Andrey <titan94@mail.ru>
2019-12-03 02:24:39 +0400

    Макрос UPDATE_FIELD

--- Red Belt/Week_1/Task_4/Task_4.cpp
+++ Red Belt/Week_1/Task_4/Task_4.cpp
@@ -1,16 +1,35 @@
-﻿#include "airline_ticket.h"
+﻿/*
+	Продолжим работу над сервисом поиска авиабилетов AviaScanner.
+	Наш сервис хранит базу данных билетов в виде vector<AirlineTicket>,
+		где AirlineTicket — такая же структура, как и в предыдущей задаче.
+	Периодически наш сервис обходит сайты авиакомпаний, собирает свежую информацию о доступных билетах и обновляет записи в своей базе данных.
+	Делается это с помощью функции void UpdateTicket(AirlineTicket& ticket, const map<string, string>& updates).
+	Параметр updates содержит пары (имя поля; новое значение).
+	При этом он содержит только те поля, которые поменялись.
+
+	Напишите макрос UPDATE_FIELD.
+	Вам дан файл airline_ticket.h, содержащий объявления структур Time, Date и AirlineTicket,
+		а также заготовка решения в виде cpp-файла update_field.cpp.
+	Пришлите на проверку cpp-файл, который
+	* подключает заголовочный файл airline_ticket.h
+	* содержит макрос UPDATE_FIELD
+	* содержит определения операторов, необходимых для считывания классов Date и Time из потока istream
+		и их использования в макросе ASSERT_EQUAL (формат ввода смотрите в юнит-тестах в файле update_field.cpp)
+*/
+
+#include "airline_ticket.h"
 #include "test_runner.h"
 
 using namespace std;
 
 #define UPDATE_FIELD(ticket, field, values)		\
-{												\
-	auto it = values.find(#field);				\
-	if (it != values.end())						\
-	{											\
-		std::istringstream is (it->second);		\
-		is >> ticket.field;						\
-	}											\
+{							\
+	auto it = values.find(#field);			\
+	if (it != values.end())				\
+	{						\
+		std::istringstream is (it->second);	\
+		is >> ticket.field;			\
+	}						\
 }
 
 void TestUpdate()
@@ -49,7 +68,8 @@ void TestUpdate()
 	ASSERT_EQUAL(t.arrival_time, (Time{ 20, 33 }));
 }
 
-int main() {
+int main()
+{
 	TestRunner tr;
 	RUN_TEST(tr, TestUpdate);
 }
