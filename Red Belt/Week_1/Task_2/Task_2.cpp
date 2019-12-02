5f55ba9c86a29fde47532da6cce582a3346bd282
Andrey <titan94@mail.ru>
2019-12-03 02:24:24 +0400

    Макрос LOG

--- Red Belt/Week_1/Task_2/Task_2.cpp
+++ Red Belt/Week_1/Task_2/Task_2.cpp
@@ -1,4 +1,24 @@
-﻿#include "test_runner.h"
+﻿/*
+	Вам дан класс Logger и макрос LOG
+
+	Параметры макроса LOG — объект класса Logger и строковое сообщение, которое должно быть передано в метод Log.
+	В зависимости от настроек объекта класса Logger логируемое сообщение должно предваряться именем файла или номером строки.
+	Смотрите юнит-тесты в заготовке решения для более подробного описания поведения.
+
+	Реализуйте макрос LOG так, чтобы он добавлял в логируемое сообщение имя файла
+		и номер строки в зависимости от настроек объекта logger. Пришлите на проверку cpp-файл, содержащий
+	* реализацию макроса LOG
+	* объявление класса Logger, при этом вы можете при необходимости добавлять в него дополнительные поля и методы
+	* определения всех методов класса Logger
+
+	Замечание
+
+	Если вы используете Visual Studio, у вас могут не проходить юнит-тесты из заготовки,
+		потому что в этой среде макрос __FILE__ раскрывается в абсолютный путь до файла.
+	Учитывайте это при отладке своего решения.
+*/
+
+#include "test_runner.h"
 #include <sstream>
 #include <string>
 using namespace std;
@@ -6,7 +26,10 @@ using namespace std;
 class Logger
 {
 public:
-	explicit Logger(ostream& output_stream) : os(output_stream) {}
+	explicit Logger(ostream& output_stream)
+		: os(output_stream)
+	{
+	}
 
 	void SetLogLine(bool value) { log_line = value; }
 	void SetLogFile(bool value) { log_file = value; }
@@ -39,8 +62,8 @@ private:
 	int line;
 };
 
-#define LOG(logger, message)	\
-{								\
+#define LOG(logger, message)		\
+{					\
 	logger.SetFile(__FILE__);	\
 	logger.SetLine(__LINE__);	\
 	logger.Log(message);		\
@@ -71,7 +94,8 @@ void TestLog()
 	ASSERT_EQUAL(logs.str(), expected);
 }
 
-int main() {
+int main()
+{
 	TestRunner tr;
 	RUN_TEST(tr, TestLog);
 }
--- Red Belt/Week_1/Task_2/test_runner.h
+++ Red Belt/Week_1/Task_2/test_runner.h
@@ -117,21 +117,21 @@ private:
 	int fail_count = 0;
 };
 
-#define ASSERT_EQUAL(x, y)					\
-{											\
-	ostringstream os;						\
-	os << #x << " != " << #y << ", "		\
-	   << __FILE__ << ":" << __LINE__;		\
-	AssertEqual(x, y, os.str());			\
+#define ASSERT_EQUAL(x, y)			\
+{						\
+	ostringstream os;			\
+	os << #x << " != " << #y << ", "	\
+	   << __FILE__ << ":" << __LINE__;	\
+	AssertEqual(x, y, os.str());		\
 }
 
-#define ASSERT(x)							\
-{											\
-	ostringstream os;						\
-	os << #x << " is false, "				\
-	   << __FILE__ << ":" << __LINE__;		\
-	Assert(x, os.str());					\
+#define ASSERT(x)				\
+{						\
+	ostringstream os;			\
+	os << #x << " is false, "		\
+	   << __FILE__ << ":" << __LINE__;	\
+	Assert(x, os.str());			\
 }
 
-#define RUN_TEST(tr, func)					\
+#define RUN_TEST(tr, func)			\
 	tr.RunTest(func, #func)
