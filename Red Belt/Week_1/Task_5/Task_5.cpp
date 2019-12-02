9db84c08acde7bc8e09723a8ea3454df856d7bce
Andrey <titan94@mail.ru>
2019-12-03 02:25:10 +0400

    Плохой макрос

--- Red Belt/Week_1/Task_5/Task_5.cpp
+++ Red Belt/Week_1/Task_5/Task_5.cpp
@@ -1,15 +1,20 @@
-﻿#include "test_runner.h"
+﻿/*
+	Вам дан макрос, который распечатывает два переданных ему значения в переданный поток вывода.
+	В реализации макроса есть недостаток, ограничивающий его применимость в реальных программах.
+	Вам требуется найти и устранить недостаток.
+*/
+#include "test_runner.h"
 
 #include <ostream>
 
 using namespace std;
 
-#define PRINT_VALUES(out, x, y)								\
-do															\
-{															\
-	out << (x) << endl; 									\
-	out << (y) << endl;										\
-	break;													\
+#define PRINT_VALUES(out, x, y)	\
+do				\
+{				\
+	out << (x) << endl; 	\
+	out << (y) << endl;	\
+	break;			\
 } while (true)
 
 int main()
