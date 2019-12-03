7cda192ffa661d024ed68258dcc7add6671f569c
Andrey <titan94@mail.ru>
2019-12-04 02:53:56 +0400

    Swap, SortPointers, ReversedCopy

---
+++ Red Belt/Week_3/Task_2/Task_2.cpp
@@ -0,0 +1,111 @@
+﻿/*
+	В этой задаче вам нужно реализовать три шаблонных функции:
+
+	*	template <typename T> void Swap(T* first, T* second) — обменивает местами значения,
+			на которые указывают указатели first и second
+	*	template <typename T> void SortPointers(vector<T*>& pointers) — сортирует указатели по значениям,
+			на которые они указывают
+	*	template <typename T> void ReversedCopy(T* src, size_t count, T* dst) — копирует в обратном порядке count элементов,
+			начиная с адреса в указателе src, в область памяти, начинающуюся по адресу dst.
+		При этом:
+		1.	каждый объект из диапазона [src; src + count) должен быть скопирован не более одного раза;
+		2.	диапазоны [src; src + count) и [dst; dst + count) могут пересекаться;
+		3.	элементы в части диапазона [src; src + count), которая не пересекается с [dst; dst + count),
+				должны остаться неизменными.
+*/
+#include "test_runner.h"
+
+#include <algorithm>
+#include <numeric>
+
+template <typename T>
+void Swap(T* first, T* second)
+{
+	std::swap(*first, *second);
+}
+
+template <typename T>
+void SortPointers(std::vector<T*>& pointers)
+{
+	std::sort(pointers.begin(), pointers.end(),
+		[](T* lhs, T* rhs)
+		{
+			return *lhs < *rhs;
+		});
+}
+
+template <typename T>
+void ReversedCopy(T* source, size_t count, T* destination)
+{
+	T* temp = new T[count];
+	std::copy(source, source + count, temp);
+	std::reverse_copy(temp, temp + count, destination);
+	delete[] temp;
+}
+
+void TestSwap()
+{
+	int a = 1;
+	int b = 2;
+	Swap(&a, &b);
+	ASSERT_EQUAL(a, 2);
+	ASSERT_EQUAL(b, 1);
+
+	std::string h = "world";
+	std::string w = "hello";
+	Swap(&h, &w);
+	ASSERT_EQUAL(h, "hello");
+	ASSERT_EQUAL(w, "world");
+}
+
+void TestSortPointers()
+{
+	int one = 1;
+	int two = 2;
+	int three = 3;
+
+	std::vector<int*> pointers;
+	pointers.push_back(&two);
+	pointers.push_back(&three);
+	pointers.push_back(&one);
+
+	SortPointers(pointers);
+
+	ASSERT_EQUAL(pointers.size(), 3u);
+	ASSERT_EQUAL(*pointers[0], 1);
+	ASSERT_EQUAL(*pointers[1], 2);
+	ASSERT_EQUAL(*pointers[2], 3);
+}
+
+void TestReverseCopy()
+{
+	const size_t count = 7;
+
+	int* source = new int[count];
+	int* dest = new int[count];
+
+	for (size_t i = 0; i < count; ++i)
+	{
+		source[i] = i + 1;
+	}
+	ReversedCopy(source, count, dest);
+	const std::vector<int> expected1 = { 7, 6, 5, 4, 3, 2, 1 };
+	ASSERT_EQUAL(std::vector<int>(dest, dest + count), expected1);
+
+	// Области памяти могут перекрываться
+	ReversedCopy(source, count - 1, source + 1);
+	const std::vector<int> expected2 = { 1, 6, 5, 4, 3, 2, 1 };
+	ASSERT_EQUAL(std::vector<int>(source, source + count), expected2);
+
+	delete[] dest;
+	delete[] source;
+}
+
+int main()
+{
+	TestRunner tr;
+	RUN_TEST(tr, TestSwap);
+	RUN_TEST(tr, TestSortPointers);
+	RUN_TEST(tr, TestReverseCopy);
+	return 0;
+}
--- Red Belt/Week_3/Task_2/test_runner.h
+++ Red Belt/Week_3/Task_2/test_runner.h
@@ -8,10 +8,8 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 template <class T>
-ostream& operator<< (ostream& os, const vector<T>& v)
+std::ostream& operator<< (std::ostream& os, const std::vector<T>& v)
 {
 	os << "{";
 	bool first = true;
@@ -28,7 +26,7 @@ ostream& operator<< (ostream& os, const vector<T>& v)
 }
 
 template <class T>
-ostream& operator<< (ostream& os, const set<T>& s)
+std::ostream& operator<< (std::ostream& os, const std::set<T>& s)
 {
 	os << "{";
 	bool first = true;
@@ -45,7 +43,7 @@ ostream& operator<< (ostream& os, const set<T>& s)
 }
 
 template <class K, class V>
-ostream& operator<< (ostream& os, const map<K, V>& m)
+std::ostream& operator<< (std::ostream& os, const std::map<K, V>& m)
 {
 	os << "{";
 	bool first = true;
@@ -62,21 +60,21 @@ ostream& operator<< (ostream& os, const map<K, V>& m)
 }
 
 template <class T, class U>
-void AssertEqual(const T& t, const U& u, const string& hint = {})
+void AssertEqual(const T& t, const U& u, const std::string& hint = {})
 {
 	if (!(t == u))
 	{
-		ostringstream os;
+		std::ostringstream os;
 		os << "Assertion failed: " << t << " != " << u;
 		if (!hint.empty())
 		{
 			os << "hint: " << hint;
 		}
-		throw runtime_error(os.str());
+		throw std::runtime_error(os.str());
 	}
 }
 
-inline void Assert(bool b, const string& hint)
+inline void Assert(bool b, const std::string& hint)
 {
 	AssertEqual(b, true, hint);
 }
@@ -85,22 +83,22 @@ class TestRunner
 {
 public:
 	template <class TestFunc>
-	void RunTest(TestFunc func, const string& test_name)
+	void RunTest(TestFunc func, const std::string& test_name)
 	{
 		try
 		{
 			func();
-			cerr << test_name << " OK " << endl;
+			std::cerr << test_name << " OK" << std::endl;
 		}
-		catch (exception& e)
+		catch (std::exception & e)
 		{
 			++fail_count;
-			cerr << test_name << " fail: " << e.what() << endl;
+			std::cerr << test_name << " fail: " << e.what() << std::endl;
 		}
 		catch (...)
 		{
 			++fail_count;
-			cerr << "Unknown exception caught" << endl;
+			std::cerr << "Unknown exception caught" << std::endl;
 		}
 	}
 
@@ -108,7 +106,7 @@ public:
 	{
 		if (fail_count > 0)
 		{
-			cerr << fail_count << " unit tests failed. Terminate" << endl;
+			std::cerr << fail_count << " unit tests failed. Terminate" << std::endl;
 			exit(1);
 		}
 	}
@@ -117,21 +115,21 @@ private:
 	int fail_count = 0;
 };
 
-#define ASSERT_EQUAL(x, y)			\
-{						\
-	ostringstream os;			\
-	os << #x << " != " << #y << ", "	\
-	   << __FILE__ << ":" << __LINE__;	\
-	AssertEqual(x, y, os.str());		\
+#define ASSERT_EQUAL(x, y)					\
+{								\
+	std::ostringstream test_runner_os;			\
+	test_runner_os << (#x) << " != " << (#y) << ", "	\
+	   << __FILE__ << ":" << __LINE__;			\
+	AssertEqual(x, y, test_runner_os.str());		\
 }
 
-#define ASSERT(x)				\
-{						\
-	ostringstream os;			\
-	os << #x << " is false, "		\
-	   << __FILE__ << ":" << __LINE__;	\
-	Assert(x, os.str());			\
+#define ASSERT(x)						\
+{								\
+	std::ostringstream test_runner_os;			\
+	test_runner_os << (#x) << " is false, "			\
+	   << __FILE__ << ":" << __LINE__;			\
+	Assert(x, test_runner_os.str());			\
 }
 
-#define RUN_TEST(tr, func)			\
+#define RUN_TEST(tr, func)					\
 	tr.RunTest(func, #func)
